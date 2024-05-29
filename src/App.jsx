52010271074c09c46309b14a72d55bbae208cccc
hamitLicina <hamitlicina@gmail.com>
2024-05-29 22:33:45 +0300

    Optimize CSS styles and update App structure: Improved root styles in index.css, enhanced component styles in App.css, and updated App.jsx for better styling consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import Button from 'react-bootstrap/Button';
-//import Alert from 'react-bootstrap/Alert';
-import { Alert } from 'react-bootstrap';
-import Accordion from 'react-bootstrap/Accordion';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
+
 
 function App() {
-  
+
 
   return (
     <>
